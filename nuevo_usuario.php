3b6df9cd752fd1b549932462c92208835e45a2e0
zallen-mobius <zallen.mobius@outlook.com>
2022-06-04 00:36:22 -0500

    Producto final

--- nuevo_usuario.php
+++ nuevo_usuario.php
@@ -4,7 +4,7 @@
 <!-- López Carranza - Medina Sifuentes -->
 <!--            (Grupo 14)             -->
 
-<html lang="en">
+<html lang="es">
     <head>
         <!-- meta -->
         <meta charset="UTF-8">
@@ -15,12 +15,12 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">     <!-- uso de Google Fonts-->
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Cabin&family=Rubik&family=Rubik+Bubbles&family=Rubik+Puddles&display=swap" rel="stylesheet">    <!-- Esta es la que se va a cambiar con cada nueva fuente. -->
-        <link rel="stylesheet" href="/src/styles/style.css">        <!-- Se carga el archivo de estilos general -->
-        <link rel="stylesheet" href="/src/styles/nuevo_usuario.css">
+        <link rel="stylesheet" href="src/styles/style.css">        <!-- Se carga el archivo de estilos general -->
+        <link rel="stylesheet" href="src/styles/nuevo_usuario.css">
     </head>
     <body>
         <header class="barra" id="menu">      <!--La sección del menu superior, usamos nav porque se usa para navegar en el sitio.-->
-            <a href="/index.html"> <img src="src/img/icon.png" alt="Aqui debería ir el logo" id="menu-logo"> </a>      <!-- Flaticon -->
+            <a href="index.html"> <img src="src/img/icon.png" alt="Aqui debería ir el logo" id="menu-logo"> </a>      <!-- Flaticon -->
             <ul id="menu-opciones">
                 <li>
                     <a href="cuatro_cero_cuatro.html"> Jugar </a>     <!-- Esta pestaña llevara a un formulario que pedira un codigo -->
@@ -59,7 +59,7 @@
                 <div id="aviso-creada-objetos">
                     <h1> ¡Ya eres parte de Schooly! </h1>
                     <div>
-                        <img src="/src/img/valores.jpeg"/>
+                        <img src="src/img/valores.jpeg"/>
                     </div>
                     <input type="button" value="Continuar" onclick="location.href='index.html'" id="boton">
                 </div>      <!-- div aviso-falta-objetos -->
@@ -74,22 +74,22 @@
             <div>
                 <ul id="scl-logos">
                     <li>
-                        <a href="https://www.facebook.com/Py-web-106771222048009"><img src="/src/img/scl-facebook.png" class="scl-logo">
+                        <a href="https://www.facebook.com/Py-web-106771222048009"><img src="src/img/scl-facebook.png" class="scl-logo">
                     </li>
                     <li>
                         <a href="#"> <img src="src/img/scl-instagram.png" class="scl-logo"> </a>
                     </li>
                     <li>
-                        <a href="https://twitter.com/webp2814"> <img src="/src/img/scl-twitter.png" class="scl-logo"> </a>
+                        <a href="https://twitter.com/webp2814"> <img src="src/img/scl-twitter.png" class="scl-logo"> </a>
                     </li>
                     <li>
-                        <a href="https://www.youtube.com/channel/UCvUPNPBSCsdwcJmB18eZpEg"> <img src="/src/img/scl-youtube.png" class="scl-logo"> </a>
+                        <a href="https://www.youtube.com/channel/UCvUPNPBSCsdwcJmB18eZpEg"> <img src="src/img/scl-youtube.png" class="scl-logo"> </a>
                     </li>
                     <li>
-                        <a href="https://www.pinterest.com.mx/webp2814/"> <img src="/src/img/scl-pinterest.png" class="scl-logo"> </a>
+                        <a href="https://www.pinterest.com.mx/webp2814/"> <img src="src/img/scl-pinterest.png" class="scl-logo"> </a>
                     </li>
                     <li>
-                        <a href="#"> <img src="/src/img/scl-googleplay.png" class="scl-logo"> </a>
+                        <a href="#"> <img src="src/img/scl-googleplay.png" class="scl-logo"> </a>
                     </li>
                 </ul>       <!-- ul scl-logos --> 
             </div>      <!-- div -->
